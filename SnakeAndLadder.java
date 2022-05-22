76b62be761eed51609acdb885538df91665af9cb
suraj <surajmore2143@gmail.com>
2022-05-22 23:33:13 +0530

    Added to check the player feature

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,31 @@
 package com.bridglabz;
 
 public class SnakeAndLadder {
-    static int no_play = 1;
-    static int ladder = 2;
-    static int snake = 3;
+    static final int no_play = 1;
+    static final int ladder = 2;
+    static final int snake = 3;
 
     public static void main(String[] args) {
         int Position = 0;
         System.out.println("Position of player at start is: " + Position);
-        int rollsDie = (int)((Math.random()*10)%6+1);
-        System.out.println("Dice value = "+rollsDie);
+        int rollsDie = (int) ((Math.random() * 10) % 6 + 1);
+        System.out.println("Dice value = " + rollsDie);
+        int playerCheck = (int) (Math.floor(Math.random() * 10) % 3) + 1;
+        switch (playerCheck) {
+            case no_play:
+                System.out.println("No Play! Your position can not be changed,you are at :  " + Position);
+                break;
+            case ladder:
+                Position = Position + rollsDie;
+                System.out.println("Congratulation!climb up the ladder:  " + Position);
+                break;
+            case snake:
+                Position = Position - rollsDie;
+                System.out.println("Oops! swallowed by snake  :  " + Position);
+                break;
+            default:
+                System.out.println("Something went wrong");
+
+        }
     }
 }
