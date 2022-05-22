cdfac17ba7504175db6828049c6434c37c43845b
suraj <surajmore2143@gmail.com>
2022-05-22 22:35:20 +0530

    Added to the initial position as zero feature

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridglabz;
+
+public class SnakeAndLadder {
+    static int no_play = 1;
+    static int ladder = 2;
+    static int snake = 3;
+
+    public static void main(String[] args) {
+        int Position = 0;
+        System.out.println("Position of player at start is " + Position);
+    }
+}
